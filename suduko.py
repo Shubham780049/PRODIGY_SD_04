a5144d80bd405f86b04a2e7eddc905df30fcbf2a
Shubham Tiwari <160890896+Shubham780049@users.noreply.github.com>
2024-08-31 12:33:03 +0530

    Add files via upload

---
+++ suduko.py
@@ -0,0 +1,82 @@
+def is_valid(board, row, col, num):
+    # Check if 'num' is not in the given row
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    start_row = row - row % 3
+    start_col = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+
+    return True
+
+
+def solve_sudoku(board):
+    empty_spot = find_empty(board)
+    if not empty_spot:
+        return True  # Puzzle solved
+    row, col = empty_spot
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0  
+
+    return False
+
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:  # Empty cell
+                return (i, j)
+    return None
+
+
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+
+# Example Sudoku puzzle (0 represents empty spaces)
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Sudoku Puzzle:")
+print_board(sudoku_board)
+
+if solve_sudoku(sudoku_board):
+    print("\nSolved Sudoku:")
+    print_board(sudoku_board)
+else:
+    print("No solution exists.")
